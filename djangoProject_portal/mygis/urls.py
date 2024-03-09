67a7fcbe00d964b24f8207a761130b8aeef258ec
Lina <kokoshalina@gmail.com>
2024-03-09 23:43:41 +0200

    mygis/urls.py was created. def home added

--- djangoProject_portal/mygis/urls.py
+++ djangoProject_portal/mygis/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path
+
+from mygis import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mygis.urls')),
+    re_path(r'^$', views.home, name='home'),
 ]
