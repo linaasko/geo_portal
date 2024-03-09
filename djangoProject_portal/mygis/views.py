6e4523ba449a886c9a42ff776e648580e42660a9
Lina <kokoshalina@gmail.com>
2024-03-10 00:39:05 +0200

    template home added

--- djangoProject_portal/mygis/views.py
+++ djangoProject_portal/mygis/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'mygis/home.html'))
+    #return HttpResponse("Hello world!")
