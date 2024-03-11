30dc49c193036995b111e7b2ba430e89717aa985
Lina <kokoshalina@gmail.com>
2024-03-11 12:02:25 +0200

    view home was modified(articles context). template home was modified(articles displayed).

--- djangoProject_portal/mygis/views.py
+++ djangoProject_portal/mygis/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from mygis.models import Articles
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'mygis/base.html')
-    #return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    # return render(request, 'mygis/home.html')
+    articles = Articles.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'mygis/home.html', context)
