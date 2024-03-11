a8f6c4b647cb0dffac4b5653d36da6a32886656f
Lina <kokoshalina@gmail.com>
2024-03-11 12:09:52 +0200

    get_short_text method was added to model Articles

--- djangoProject_portal/mygis/models.py
+++ djangoProject_portal/mygis/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+SHORT_TEXT_LEN = 450
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +8,9 @@ class Articles(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     def __str__(self):
         return self.title
+
+    def get_short_text(self):
+        if len(self.text) > SHORT_TEXT_LEN:
+            return self.text[:SHORT_TEXT_LEN]
+        else:
+            return self.text
